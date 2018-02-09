06f546761833b7011c0d2c2bf1063d587d70b1fd
mwalls1 <walls594@gmail.com>
2018-02-09 12:20:24 -0600

    Refreshed repository

--- project4/src/postage2/PostageUtil.java
+++ project4/src/postage2/PostageUtil.java
@@ -13,6 +13,8 @@ public class PostageUtil
   {
     if(weight<=1)
     	return .47;
+    else if(weight>1)
+    	return .47+Math.ceil(weight-1)*.21;
     else if(weight>3.5)
     	return .94+Math.ceil(weight-1)*.21;
     else
